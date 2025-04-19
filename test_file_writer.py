cab2bae1b3247a8c8ef83df3460cb394bcae1daf
Nikhil J <nikhil89041@gmail.com>
2025-04-19 17:34:24 +0530

    code changed

--- test_file_writer.py
+++ test_file_writer.py
@@ -2,8 +2,9 @@ import os
 from file_writter import write_to_file
 
 def test_write_to_file(tmp_path):
-    tmp_path = r"C:\Users\admin\OneDrive\Desktop\livecicd"
+    
     test_text = "Hello, pytest!"
+    test_content = "Hello, pytest!"
     test_file = tmp_path / "test_output.txt"
 
     write_to_file(test_text, filename=str(test_file))
