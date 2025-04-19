0720b45e60f133fb59f69ec0728002f31263e8b5
Nikhil J <nikhil89041@gmail.com>
2025-04-19 17:36:23 +0530

    code changed

--- test_file_writer.py
+++ test_file_writer.py
@@ -1,10 +1,7 @@
-import os
 from file_writter import write_to_file
 
 def test_write_to_file(tmp_path):
-    
     test_text = "Hello, pytest!"
-    test_content = "Hello, pytest!"
     test_file = tmp_path / "test_output.txt"
 
     write_to_file(test_text, filename=str(test_file))
