d7d886bee524d89f43025e248175513c0346007e
Nikhil J <nikhil89041@gmail.com>
2025-04-19 17:40:56 +0530

    code changed

--- file_writter.py
+++ file_writter.py
@@ -1,6 +1,8 @@
-text = str(input("Enter the text to write to the file: "))
-def write_to_file(text):
-    with open("output.txt", "w") as file:
+def write_to_file(text, filename="output.txt"):
+    with open(filename, "w") as file:
         file.write(text)
-write_to_file(text)
-print("Text written to output.txt successfully.")
+
+if __name__ == "__main__":
+    text = input("Enter the text to write to the file: ")
+    write_to_file(text)
+    print("Text written to output.txt successfully.")
