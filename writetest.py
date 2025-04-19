fa0a3b30b663bbc9530238c4fbdcd011eb34b006
Nikhil J <nikhil89041@gmail.com>
2025-04-19 17:16:39 +0530

    changes done

--- writetest.py
+++ writetest.py
@@ -1,16 +1,19 @@
 from file_writter import write_to_file
 
 def test_write_to_file():
-    
-    write_to_file("test.txt", "Hello, World!")
-    with open("test.txt", "r") as file:
+    write_to_file("Hello, World!")
+    with open("output.txt", "r") as file:
         content = file.read()
     assert content == "Hello, World!"
 
-    
-    write_to_file("test.txt", "This is a test.")
-    with open("test.txt", "r") as file:
+          
+    write_to_file("This is a test.")
+    with open("output.txt", "r") as file:
         content = file.read()
     assert content == "Hello, World!This is a test."
+
+
+    
+ 
 
    
